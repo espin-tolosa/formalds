1a35efbf63f9a2e401c4730d2efa939c321801c2
sespinto <41337564+espin-tolosa@users.noreply.github.com>
2024-07-17 00:24:00 +0200

    Update test_math.c

--- test_math.c
+++ test_math.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 
+typedef float f32_t;
+typedef unsigned int u32_t;
+typedef _Bool bool_t;
+
+
 #define MY_INF ( 1.0f / 0.0f )
 #define MY_NAN ( MY_INF - MY_INF )
 
@@ -19,6 +24,7 @@
 
 double my_exp_imp( double x );
 double my_log_imp( double x );
+f32_t my_exp_legacy( f32_t x );
 
 typedef struct
 {
@@ -148,37 +154,7 @@ double my_abs( double x )
 	return x < 0.0 ? -x : x;
 }
 
-/* x = g + k * ln2 */
-normalized_fp_t normalize_exp( double x )
-{
-	normalized_fp_t gn;
 
-	const double half_ln2	= 0.34657359027997265470861606072909; /* ln(2) / 2 */
-	const double ln2		= 0.69314718055994530941723212145818; /* ln(2) */
-
-    if( x < 0.0f )
-    {
-        x = -x;
-        gn.fsign = -1;
-    }
-    else
-    {
-        gn.fsign = +1;
-    }
-
-	gn.f = x;
-	gn.e = 0;
-	gn.esign = 1;
-	gn.B = ln2;
-	
-	while( my_abs(gn.f) > half_ln2 )
-	{
-		gn.e++;
-		gn.f = x - gn.e * ln2;
-	}
-	
-	return (gn);
-}
 
 fp_types_t get_type( float x )
 {
@@ -289,32 +265,90 @@ double my_exp( double x )
 	return result;
 }
 
+/* x = g + k * ln2 */
+normalized_fp_t normalize_exp( double x )
+{
+	normalized_fp_t gn;
+
+	const double half_ln2	= 0.34657359027997265470861606072909; /* ln(2) / 2 */
+	const double ln2		= 0.69314718055994530941723212145818; /* ln(2) */
+
+    if( x < 0.0f )
+    {
+        x = -x;
+        gn.fsign = -1;
+    }
+    else
+    {
+        gn.fsign = +1;
+    }
+
+	gn.f = x;
+	gn.e = 0;
+	gn.esign = 1;
+	gn.B = ln2;
+	
+	while( my_abs(gn.f) > half_ln2 ) // f = 0.40 > 0.34
+	{
+		gn.e++;
+		gn.f = x - gn.e * ln2;
+	}
+	
+	return (gn);
+}
+
 /*
  * x = g + e * ln2; exp(x) = 2**e * exp(g)
  */
 
 double my_exp_imp( double x )
-{
-	normalized_fp_t gn = normalize_exp( x );
-
-	double g = gn.f;
+{	
+	int neg;
+	
+	if( x < 0.0 )
+	{
+		x = -x;
+		neg = 1;
+	}
+	else
+	{
+		neg = 0;
+	}
 
 	/* Cody & Waite, Chapter 6, page 69 */
-	const double p0 = 0.25000000000000000000e+0;
-	const double p1 = 0.75753180159422776666e-2;
-	const double p2 = 0.31555192765684646356e-4;
-	const double q0 = 0.50000000000000000000e+0;
-	const double q1 = 0.56817302698551221787e-1;
-	const double q2 = 0.63121894374398503557e-3;
-	const double q3 = 0.75104028399870046114e-6;
+	const double p0 	= 0.25000000000000000000e+0;
+	const double p1 	= 0.75753180159422776666e-2;
+	const double p2 	= 0.31555192765684646356e-4;
+	const double q0 	= 0.50000000000000000000e+0;
+	const double q1 	= 0.56817302698551221787e-1;
+	const double q2 	= 0.63121894374398503557e-3;
+	const double q3 	= 0.75104028399870046114e-6;
+	const double invln2 = 1.442695040888963407;
+	const double c1 	= 22713.0 / 32768.0;
+	const double c2 	= 1.428606820309417232e-6;
+
+	/* x = g + e * ln2 */
+	const int  		e = (int)(x * invln2 + 0.5);
+	const double 	g = (x - (double)e * c1) - (double)e * c2; /* Cody & Waite, Chapter 6, page 68, machines with guard digit */
 
 	const double z = g * g;
 	
-	const double gP = ((p2 * z + p1) * z + p0 ) * g;
-	const double Q  = (q2 * z + q1) * z + q0;
-	const double R  = 0.5 + gP / ( Q - gP );
+	const double gP = g * ( (p2 * z + p1) * z + p0 );
+	const double Q  = (q3 * z + q2) * z + q1;
+	const double R 	= 0.5 + gP / (Q * z + q0 - gP);
 
-	return R * ( 1LLU << ( 1LLU + gn.e ) );
+	double k2 = 1.0;
+	
+	for( int i = 0; i < (1 + e); ++i ) { k2 = 2.0 * k2;	}
+
+	if( neg )
+	{
+		return 1.0 / ( R * k2 );
+	}
+	else
+	{
+		return R * k2;
+	}
 }
 
 /* x**y = e**(yln(x)) */
@@ -326,13 +360,45 @@ double my_pow_imp( double x, double y )
 	return ( my_exp( w ) );
 }
 
+f32_t my_exp_legacy( f32_t x )				// x 	= -1.0;
+{
+	bool_t neg = x < 0.0f;
+	if( neg )
+	{
+		x = -x;
+	}
+
+	f32_t term		= x; 					// term = -1.0;
+	u32_t i			= 2;					// i	= 2;
+	f32_t result	= 1.0f + x;				// res 	= 0.0;
+	bool_t tol		= term < 1.0E-5f;		// tol  = 
+	
+	while( !tol && ( i < 300 ) )
+	{
+		term 	= term / (f32_t) i;
+		term 	= term * x;
+		result 	= result + term;
+		i 		= 1u + i;
+		tol		= term < 1.0E-5f;
+	}
+	
+	if( neg )
+	{
+		return ( 1.0 / result );
+	}
+	else
+	{
+		return ( result );
+	}
+}
+
 int main ( void )
 {
 	float x;
 	fp_types_t y;
 	
 	normalized_fp_t xn;
-	
+#if 1
 	x = 1.e38; printf("LOG(%f): %f, %f\n",x, logf( x ), my_log_imp( x ));
 
 	x = -0.0001123; xn = normalize_exp( x );
@@ -345,7 +411,22 @@ int main ( void )
 	x = 25.0; printf("EXP(%f): %f, %f\n",x, exp( x ), my_exp( x ));
 	
 	x = 25.0; printf("POW(%f, 7.0): %f, %f\n",x, pow( x, 7.0 ), my_pow_imp( x, 7.0 ));
+	x = 50.0; printf("POW(%f, 7.0): %f, %f\n",x, pow( x, 7.0 ), my_pow_imp( x, 7.0 ));
 
+	x = 25.0; printf("EXP(%f): %f, %f, diff = %f\n", x, exp( x ), my_exp_legacy( x ), fabs(exp( x ) - my_exp_legacy( x )) );
+	x = 5.0; printf("EXP(%f) * EXP(-%f) = 1.0: exp(x) = %f, exp(-x) = %f, x= %f, diff = %f\n", x, x, exp( x ), exp( -x ), exp( x )*exp( -x ), fabs(1.0f - exp( x )*exp( -x )) );
+	x = 5.0; printf("EXP(%f) * EXP(-%f) = 1.0: exp(x) = %f, exp(-x) = %f, x= %f, diff = %f\n", x, x, my_exp( x ), my_exp( -x ), my_exp( x )*my_exp( -x ), fabs(1.0f - my_exp( x )*my_exp( -x )) );
+	x = 5.0; printf("EXP(%f) * EXP(-%f) = 1.0: exp(x) = %f, exp(-x) = %f, x= %f, diff = %f\n", x, x, my_exp_legacy( x ),my_exp_legacy( -x ), my_exp_legacy( x )*my_exp_legacy( -x ), fabs(1.0f - my_exp_legacy( x )*my_exp_legacy( -x )) );
+#endif
+
+#if 0
+	printf("x,                 my_exp,         math_H,         my_legacy\n");
+	for(int i = -100; i <= 100; i++)
+	{
+		double delta = 1.e-12;
+		printf("%.12f, %.12f, %.12f, %.12f\n", (double) i + delta, (my_exp( (double) i + delta )), (exp( (double) i + delta )), (my_exp_legacy( (double) i + delta )) );
+	}
+#endif
 
 #if 0	
 	x = 0.0f; y = get_type( x ); printf("%.1f\tis\tZERO=%d,\tINF=%d,\tNAN=%d,\tFINITE=%d,\tSIGN=:%c1\n",x, y.is_zero, y.is_inf, y.is_nan, y.is_finite, y.sign  );
